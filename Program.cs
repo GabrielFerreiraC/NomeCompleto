d88efae17317343ddf0791088f989f7cd7fed6a4
Gabriel Ferreira <gfdc2204@gmail.com>
2022-09-16 19:36:55 -0300

    adiciona a solução do exercício

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿string nome, sobrenome; 
+Console.Write("Digite seu nome......:");
+nome = Console.ReadLine(); 
+
+Console.Write ("Digite seu sobrenome....:");
+sobrenome = Console.ReadLine();
+
+Console.WriteLine (); 
+
+Console.WriteLine($"{nome} {sobrenome} "); 
+Console.WriteLine($"{sobrenome.ToUpper()}, {nome}"); 
+
+
+
+
+
+
+ 
